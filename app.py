a261ad02a75790352179e28ed9e8527e941acea9
kbfreder <kbfreder@yahoo.com>
2023-05-21 17:22:19 -0600

    rename main.py --> app.py; clean up code

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 
 import os
+
 from flask import (
     Flask, render_template, request, redirect, g
 )
 from werkzeug.utils import secure_filename
 
 from src.cv import ScreenShotAnalysis
+
 
 cv = ScreenShotAnalysis()
 
@@ -28,30 +30,18 @@ def upload_image():
         img_path = os.path.join(BASE_DIR, app.config['IMAGE_UPLOADS'], filename)
         image.save(img_path)
         g.filename = filename
-        # text = cv.process(img_path)
-        # return render_template("main.html", filename=filename, text=text)
-        return render_template("dev2.html", filename=filename)
+        return render_template("main.html", filename=filename)
 
-    return render_template("dev2.html")
+    return render_template("main.html")
 
-# @app.route("/display/<filename>")
-# def display_image(filename):
-#     return redirect(url_for('static', filename='/images/'+filename), code=301)
 
-# @app.route("/analyze/<filename>", methods=["GET, POST"])
-# def analyze_image(filename):
 @app.route("/analyze", methods=["GET", "POST"])
 def analyze_image():
-    # print(request)
-    # print(request.url)
-    # print(request.args)
-    # print(request.args.get('filename'))
-
     # we already sanitized this when image was uploaded
     filename = request.args.get('filename')
     img_path = os.path.join(BASE_DIR, app.config['IMAGE_UPLOADS'], filename)
     text = cv.process(img_path)
-    return render_template("dev2.html", filename=filename, text=text)
-    # return render_template("dev2.html")
+    return render_template("main.html", filename=filename, text=text)
+
 
 app.run(port=5000, debug=True)
